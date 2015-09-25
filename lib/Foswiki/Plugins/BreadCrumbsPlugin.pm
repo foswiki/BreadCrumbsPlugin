cb7928ce8e8213d858a11b263ebfbb6ed47e35e2
MichaelDaum <daum@michaeldaumconsulting.com>
2015-09-25 15:24:20 +0200

    Item13750: repackage using new data form

--- lib/Foswiki/Plugins/BreadCrumbsPlugin.pm
+++ lib/Foswiki/Plugins/BreadCrumbsPlugin.pm
@@ -1,6 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2006-2014 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2006-2015 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -18,7 +18,7 @@ use strict;
 use warnings;
 
 our $VERSION = '2.51';
-our $RELEASE = '2.51';
+our $RELEASE = '23 Apr 2014';
 our $NO_PREFS_IN_TOPIC = 1;
 our $SHORTDESCRIPTION = 'A flexible way to display breadcrumbs navigation';
 our $doneInit = 0;
